89fee903e05be6f7686782014e4e4cb1713c7cfc
Abhishek <projects.mindiii@gmail.com>
2022-04-27 11:05:25 +0530

    test 1

---
+++ about.js/about.js
@@ -0,0 +1,85 @@
+import "./Home.css";
+import { Container, Row, Col } from "react-bootstrap";
+import HomeFooter from "../HomeFooter/HomeFooter";
+import HomeHeader from "../HomeHeader/HomeHeader";
+import Banner from "./Banner/Banner";
+import VideoContainer from "./VideoContainer/VideoContainer";
+import BuildPopPup from "./HomePopPup/BuildPopUp";
+import ImprovePopUp from "./HomePopPup/ImprovePopUp";
+import FallPopUp from './HomePopPup/FallPopUp';
+
+const Home = () => {
+  return (
+    <>
+      <section>
+        <div>
+          <HomeHeader />
+        </div>
+        <div>
+          <Banner />
+        </div>
+        <section className="mainSection">
+          <Container>
+            <div className="mainSectionHead">
+              <h2>BOOST PERFORMANCE EFFORTLESSLY</h2>
+              <p>
+                Maximum benefits through science-backed breathing AI.
+                <br />
+                Drug-free, safe & effective.
+              </p>
+            </div>
+            <Row>
+              <Col lg={4}>
+                <div className="icon">
+                    <BuildPopPup/>
+                  <h5>Build Stress Resilience</h5>
+                </div>
+              </Col>
+              <Col lg={4}>
+                <div className="icon">
+                 <ImprovePopUp/>
+                  <h5>Improve Task Focus</h5>
+                </div>
+              </Col>
+              <Col lg={4}>
+                <div className="icon">
+                  <FallPopUp/>
+                  <h5>Fall Asleep Easier</h5>
+                </div>
+              </Col>
+            </Row>
+          </Container>
+        </section>
+        <div>
+          <VideoContainer />
+        </div>
+        <div className="downloadSection">
+          <Container>
+            <Row>
+              <div className="footerSignUp">
+                <h4>GET EARLY ACCESS</h4>
+                <div className="signUp">
+                  <form autoComplete="off">
+                    <div>
+                      <label for="email"></label>
+                      <input
+                        type="email"
+                        name="email"
+                        placeholder="Enter Email Address"
+                      />
+                    </div>
+                    <div>
+                      <button type="submit">SIGN UP</button>
+                    </div>
+                  </form>
+                </div>
+              </div>
+            </Row>
+          </Container>
+        </div>
+      </section>
+      <HomeFooter />
+    </>
+  );
+};
+export default Home;
