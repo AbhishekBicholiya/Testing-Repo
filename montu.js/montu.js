c6171a12bdf6456670885fa791e6b7b59be529b1
AbhishekBicholiya <97518906+AbhishekBicholiya@users.noreply.github.com>
2022-04-28 14:46:01 +0530

    Update montu.js

--- montu.js/montu.js
+++ montu.js/montu.js
@@ -4,6 +4,22 @@ import { SHOW_ERR, CLEAR_ERR } from "./types";
 export function showErr(msg) {
   return { type: SHOW_ERR, msg };
 }
+export function getProductsFromApi(token) {
+	return async function(dispatch) {
+		try {
+			const products = await SublyApi.getStripeProducts(token);
+			dispatch(getProducts(products));
+		} catch (err) {
+			dispatch(showErr(err));
+		}
+	};
+}
+function getProducts(products) {
+	return {
+		type     : FETCH_PRODUCTS,
+		products
+	};
+}
 export function clearErr() {
   return { type: CLEAR_ERR };
 }
